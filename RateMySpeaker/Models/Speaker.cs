8fff01f3e9f995693973badb13df0b62429cc8ed
Paige <paige.fenwick@att.net>
2016-04-26 23:45:21 -0400

    Initial commit

---
+++ RateMySpeaker/Models/Speaker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace RateMySpeaker.Models
+{
+    public class Speaker
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter the name of the speaker.")]
+        [Display(Name = "Speaker Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the name of the conference where you saw the speaker.")]
+        [Display(Name = "Conference")]
+        public string Conference { get; set; }
+
+        [Required(ErrorMessage = "Please rate the speaker on a scale of 1 to 5")]
+        [Range(1, 5)]
+        [Display(Name = "Rating")]
+        public int Rating { get; set; }
+
+        [Required(ErrorMessage = "Please review the speaker in less than 500 characters.")]
+        [Display(Name = "Review")]
+        [StringLength(500)]
+        public string Review { get; set; }
+
+        
+    }
+
+    public class SpeakerDBContext : DbContext
+    {
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
---
+++ RateMySpeaker/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RateMySpeaker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RateMySpeaker")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35e5f67e-ee0c-4580-9718-96ed8484e550")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
